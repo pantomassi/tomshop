9dd5e7f7bd4fedea4a274eb37f3654001290f46f
Pantomassi <tmusial10@gmail.com>
2022-12-25 16:04:37 +0100

    Add routing & replace old hrefs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,27 @@
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <Container>
-        <main className="py-3">
-          <HomeScreen />
-        </main>
-      </Container>
+
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+
       <Footer />
-    </div>
+    </Router >
   );
 }
 
