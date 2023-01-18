d3a614fb69b5882e9119c6470ee42a25a9653f35
Pantomassi <tmusial10@gmail.com>
2023-01-18 15:55:34 +0100

    Make cartScreen dispatch addToCart action

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link, useParams, useSearchParams } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card, Form } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { addToCart } from '../actions/cartActions'
+import Message from '../components/Message'
+
 
 function CartScreen() {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const params = useParams()
+    const productId = params.id
+    const qty = Number(searchParams.get('qty'))
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty))
+        }
+    }, [dispatch, productId, qty])
+
     return (
         <div>
             Cart
