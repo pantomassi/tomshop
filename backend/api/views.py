397ed23f64e16842af1c388291710af4bc94a807
Pantomassi <tmusial10@gmail.com>
2023-01-23 02:45:57 +0100

    Restrict users/ & users/profile routes in views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from django.contrib.auth.models import User
 from .models import Product
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithRefreshToken
@@ -40,9 +41,18 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 
